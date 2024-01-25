843bfc56d7f96c6bcb3fa86279856c976b5d1309
dxara10 <135165192+dxara10@users.noreply.github.com>
2024-01-25 00:50:41 -0300

    Update urls.py

--- study_async/urls.py
+++ study_async/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
+import usuarios
+from usuarios.views import cadastro
+
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("usuarios/", include('usuarios.urls'))
-]
+    path('admin/', admin.site.urls),
+    path('usuarios/', include('usuarios.urls')),
+    path('flashcard/', include('flashcard.urls')),
+    path('apostilas/', include('apostilas.urls')),
+    path('', include(usuarios.urls)),
+
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
